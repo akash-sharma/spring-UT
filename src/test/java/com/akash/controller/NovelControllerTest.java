6a089392339553c780b7f84643826cf5242dabf3
akash6sharma <akash6.sharma@paytm.com>
2020-08-29 02:16:19 +0530

    cassandra read write operations Unit tests

--- src/test/java/com/akash/controller/NovelControllerTest.java
+++ src/test/java/com/akash/controller/NovelControllerTest.java
@@ -1,13 +1,21 @@
 package com.akash.controller;
 
 import com.akash.SpringUtApplication;
+import com.akash.cassandra.dao.NovelDao;
+import com.akash.cassandra.entity.Novel;
 import org.cassandraunit.spring.CassandraUnit;
 import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
 
@@ -18,4 +26,47 @@ import static org.springframework.test.context.TestExecutionListeners.MergeMode.
     listeners = CassandraUnitTestExecutionListener.class,
     mergeMode = MERGE_WITH_DEFAULTS)
 @CassandraUnit
-public class NovelControllerTest {}
+public class NovelControllerTest {
+
+  @Autowired private NovelController novelController;
+
+  @Autowired private NovelDao novelDao;
+
+  @Test
+  public void findNovelTest() {
+
+    final String author = "A1";
+    final String category = "C1";
+    novelController.save(author, category);
+    List<Novel> allByCategoryAndAuthor = novelDao.findAllByCategoryAndAuthor(category, author);
+    Assertions.assertFalse(CollectionUtils.isEmpty(allByCategoryAndAuthor));
+    final Novel novel = allByCategoryAndAuthor.get(0);
+    Assertions.assertNotNull(novel);
+    Assertions.assertEquals(novel.getAuthor(), author);
+    Assertions.assertEquals(novel.getCategory(), category);
+  }
+
+  @Test
+  public void saveNovelTest() {
+
+    final String author = "A2";
+    final String category = "C2";
+    final String genre = "fun";
+
+    Novel novel = new Novel();
+    novel.setAuthor(author);
+    novel.setCategory(category);
+    novel.setGenre(genre);
+    novel.setName("mecbeth");
+    novelDao.upsert(novel);
+
+    final String allByCategoryAndAuthor =
+        novelController.findAllByCategoryAndAuthor(author, category);
+
+    Assertions.assertNotNull(allByCategoryAndAuthor);
+    Assertions.assertTrue(allByCategoryAndAuthor.contains(author));
+    Assertions.assertTrue(allByCategoryAndAuthor.contains(category));
+    Assertions.assertTrue(allByCategoryAndAuthor.contains(genre));
+    Assertions.assertTrue(allByCategoryAndAuthor.contains("saleCount=1"));
+  }
+}
